f4b1f64a4477e7e5845cef8a7a6cc681cf8de861
ericet <ericetchen@gmail.com>
2023-11-30 05:24:20 +0800

    bug fix

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-    steem_rpc:'https://api.steememory.com',
+    steem_rpc:'https://api.campingclub.me',
     trail_api:'https://trail-api.steemcn.xyz',
     trail_account:'cn-trail'
 }
